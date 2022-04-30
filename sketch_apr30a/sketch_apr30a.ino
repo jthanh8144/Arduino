62976c4bd737d12a0e8fb8eee86c02b6a0fbcb76
VoVanThanh <thanhvo618@gmail.com>
2022-04-30 21:57:54 +0700

    done control through wifi

---
+++ sketch_apr30a/sketch_apr30a.ino
@@ -0,0 +1,56 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <cstdlib>
+#include <ctime>
+
+const char* ssid = "Really?";
+const char* password = "1toi9boso8";
+ESP8266WebServer server(80);
+
+void home() {
+  server.send(200, "application/json", "{\"status\": \"true\"}");
+}
+
+void bat() {
+  server.send(200, "application/json", "{\"status\": \"true\"}");
+  Serial.println("bat");
+}
+
+void tat() {
+  server.send(200, "application/json", "{\"status\": \"true\"}");
+  Serial.println("tat");
+}
+
+void send() {
+  srand(time(NULL));
+	int res = rand();
+  String json = "{\"temperature\": \"" + String(res) + "\"}";
+  server.send(200, "application/json", json);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("setup done");
+
+  server.on("/", home);
+  server.on("/up", bat);
+  server.on("/down", tat);
+  server.on("/send", send);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+  // delay(3000);
+}
